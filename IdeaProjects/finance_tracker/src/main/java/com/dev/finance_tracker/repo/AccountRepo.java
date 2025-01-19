33d911208458e1761af28a9fc35cf527df49f255
Gisele <121085656+Gisele-123@users.noreply.github.com>
2025-01-19 08:36:30 -0800

    Delete IdeaProjects/finance_tracker directory

--- IdeaProjects/finance_tracker/src/main/java/com/dev/finance_tracker/repo/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.dev.finance_tracker.repo;
-import com.dev.finance_tracker.model.Account;
-import  org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AccountRepo extends  JpaRepository<Account, Long>{
-}
--- IdeaProjects/finance_tracker/src/main/java/com/dev/finance_tracker/repo/BudgetRepo.java
+++
@@ -1,7 +0,0 @@
-package com.dev.finance_tracker.repo;
-import com.dev.finance_tracker.model.Budget;
-import  org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BudgetRepo extends  JpaRepository<Budget, Long>{
-}
